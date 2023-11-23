19544c80e1b05b93e46e947e79f5f853d753b1ec
Rafael <francorfs@icloud.com>
2023-11-23 00:01:40 -0300

    Atualizacao

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(rotas)
 
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(process.env.PORT, () => {
     console.log(`SERVER IS RUNNING ${port}`);
